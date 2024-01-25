67939eaaa2fdcaebd91c87f8818c5a3199467a67
a-7db <Ahmed97874@gmail.com>
2024-01-26 00:16:19 +0300

    Add project files.

---
+++ FakeData/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeData
+{
+    public static class Helper
+    {
+        public static void Print<T>(this IEnumerable<T> source, string title)
+        {
+            if (source == null)
+                return;
+            Console.WriteLine();
+            Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+            Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
+            Console.WriteLine("└───────────────────────────────────────────────────────┘");
+            Console.WriteLine();
+            foreach (var item in source)
+                Console.WriteLine(item);
+        }
+
+        public static IEnumerable<Employee> Fillter(this IEnumerable<Employee> source, Func<Employee, bool> func)
+        {
+            foreach (Employee emp in source)
+            {
+                if (func(emp))
+                {
+                    yield return emp;
+                }
+            }
+        }
+    }
+}
---
+++ delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using FakeData;
+
+namespace CADelegate;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var emps = Repository.LoadEmployees();
+
+        var startWith = GetEmployeesWith(emps, e => e.FirstName.ToLowerInvariant().StartsWith("ma"));
+        Helper.Print(startWith, "Employees Names starts with MA");
+
+        var hr = emps.Fillter(e => e.Department.ToLowerInvariant() == "hr");
+        hr.Print("HR Employees");
+    }
+
+    static IEnumerable<Employee> GetEmployeesWith(IEnumerable<Employee> sourse, Func<Employee, bool> predicate)
+    {
+        foreach (Employee emp in sourse)
+        {
+            if (predicate(emp))
+            {
+                yield return emp;
+            }
+        }
+    }
+}
