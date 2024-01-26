bf4373e49779adc81f0dd6d15c066856808f1104
a-7db <Ahmed97874@gmail.com>
2024-01-27 02:08:30 +0300

    done for day 2

---
+++ CACoreOfLinq/Program.cs
@@ -0,0 +1,64 @@
+﻿using FakeData;
+
+namespace CACoreOfLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employyes = Repository.LoadEmployees();
+
+            // LINQsyntax(employyes);
+            // ImmediateExecution();
+            LINQexamples(employyes);
+            
+            Console.ReadKey();
+        }
+
+        private static void LINQexamples(IEnumerable<Employee> employyes)
+        {
+            var empMale = employyes.Where(e => e.Gender == "male");
+
+            var empHR = employyes.Where(x => x.Salary >= 300_300);
+
+            var both = empMale.Where(x => x.Salary >= 300_300);
+            both.Print("Male & HR employees");
+        }
+
+        private static void ImmediateExecution()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+
+            numbers.Add(10);
+            numbers.Add(12);
+            numbers.Remove(4);
+
+            // It will print { 2, 6, 8, 10, 12 }
+            // because it won't iterate before do all changes on numbers,
+            // so will include the updated list
+            // this called [[ Immediate Execution ]]
+            foreach (var number in evenNumbers)
+            {
+                Console.Write($" {number}");
+            }
+        }
+
+        private static void LINQsyntax(IEnumerable<Employee> employyes)
+        {
+            // METHOD SYNTAX
+            var maleAndItEmps = employyes.Where(emp => emp.Department.ToLowerInvariant() == "it"
+            && emp.Gender.ToLowerInvariant() == "male");
+
+            maleAndItEmps.Print("IT & Male Employees");
+
+            // QUERY SYNTAX
+            var querySyntax = from emp in employyes
+                              where emp.Department.ToLower() == "it"
+                              && emp.Gender.ToLower() == "female"
+                              select emp;
+            querySyntax.Print("With Query Syntax");
+        }
+    }
+}
---
+++ CAProjectionOperatration/Program.cs
@@ -0,0 +1,95 @@
+﻿
+using FakeData02;
+
+namespace CAProjectionOperatration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Repository.LoadEmployees();
+
+            // RunExample01(employees);
+            // RunExample02();
+            // RunExample03(employees);
+            // RunExample04();
+            RunExample05(employees);
+
+            Console.ReadKey();
+        }
+
+        private static void RunExample05(IEnumerable<Employee> employees)
+        {
+            // Create teams via Zip()
+            Employee[] emps = employees.ToArray();
+
+            var firstEmps = emps[..3];
+            var lastEmps = emps[^3..];
+
+            var teams = firstEmps.Zip(lastEmps, (team1, team2) => $"{team1.FullName} -VS- {team2.FullName}");
+
+            foreach (var team in teams)
+            {
+                Console.WriteLine(team);
+            }
+        }
+
+        private static void RunExample04()
+        {
+            string[] colorName = { "Red", "Green", "Blue" };
+            string[] colorHex = { "FF0000", "00FF00", "0000FF" };
+
+            var colors = colorName.Zip(colorHex, (cName, cHex) => $"{cName} => {cHex}");
+
+            foreach ( var color in colors )
+            {
+                Console.WriteLine( color );
+            }
+        }
+
+        private static void RunExample03(IEnumerable<Employee> employees)
+        {
+            // Method Syntax
+            var allSkills = employees.SelectMany(e => e.Skills).Distinct();
+
+            // Query Syntax
+            var skills = (from emp in employees
+                         from skill in emp.Skills
+                         select skill).Distinct();
+                         
+
+            foreach (var skill in skills) { Console.WriteLine(skill); }
+        }
+
+        private static void RunExample02()
+        {
+            string[] sentences =
+            {
+                "i love asp.net core",
+                "i like sql server programming",
+                "in general i love programming"
+            };
+
+            var result = sentences.SelectMany(x => x.Split(' '));
+            
+            foreach (var c in result) { Console.WriteLine(c); }
+        }
+
+        private static void RunExample01(IEnumerable<Employee> employees)
+        {
+            var data = employees.Where(x => x.Skills.Count() < 3).Select(e =>
+            { // take some cols & add them in new object
+                return new 
+                {
+                    Name = e.FirstName + " " + e.LastName,
+                    SkillCount = e.Skills.Count()
+                };
+            });
+
+            foreach (var e in data)
+            {
+                Console.WriteLine($"{e.Name} [{e.SkillCount}]");
+            }
+        }
+    }
+}
